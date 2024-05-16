d48df72a5c75e1e0c9f51462ed1902c8e58a3bdd
Raein Nobakht <rnobakht@myseneca.ca>
2024-05-16 15:24:51 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#Raein Nobakht
+#OPS445
+
+#Objects
+
+x = 10
+y = 2
+z = 5
+
+q = x + y * z
+
+#Output
+
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(q)) 
